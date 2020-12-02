6a6ba9bdee3e5fbe05fe3018ffb2755c6810d6d0
miaolq <1975475365@qq.com>
2020-12-02 23:56:12 +0800

    fiber

--- step/didact.js
+++ step/didact.js
@@ -22,9 +22,8 @@ function createTextElement(text) {
   }
 }
 
-// todo: add update delete
-function render(element, container) {
-  const { type, props } = element
+function createDom(fiber) {
+  const { type, props } = fiber
   const dom = type === txtEle ? document.createTextNode('') : document.createElement(type)
   const isProperty = (key) => key !== 'children'
   Object.keys(props)
@@ -32,25 +31,63 @@ function render(element, container) {
     .forEach((name) => {
       dom[name] = props[name]
     })
-  props.children.map((child) => render(child, dom))
-  container.appendChild(dom)
+  return dom
 }
-
-const Didact = {
-  createElement,
-  createTextElement,
-  render,
-}
-
-/** @jsx Didact.createElement */
-const element = (
-  <div id="foo">
-    <a href="">bar</a>
-    <b />
-  </div>
-)
 
 let nextUnitOfWork = null
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+}
+
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+    index++
+  }
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
 
 function workLoop(deadline) {
   let shouldYield = false
@@ -60,11 +97,22 @@ function workLoop(deadline) {
   }
   window.requestIdleCallback(workLoop)
 }
+
 window.requestIdleCallback(workLoop)
 
-function performUnitOfWork() {
-  // todo
+const Didact = {
+  createElement,
+  createTextElement,
+  render,
 }
 
+/** @jsx Didact.createElement */
+const reactElement = (
+  <div id="foo">
+    <a href="">bar</a>
+    <b />
+  </div>
+)
+
 const container = document.getElementById('root')
-Didact.render(element, container)
+Didact.render(reactElement, container)
